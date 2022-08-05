35fd76306bd7787bdfdf97da744483e73982800b
Allan Gabriel Ramos da Silva <allan.silva@rain.us>
2022-08-05 12:43:39 -0300

    feat(tests): add jest + testing library

---
+++ src/components/input/input.test.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+
+import { SearchIcon } from '@chakra-ui/icons'
+import { render, screen } from '@testing-library/react'
+
+import { Input } from './input'
+import type { InputProps } from './input'
+
+const selectors = {
+  icon: () => screen.getByTestId('input-icon'),
+  placeholder: () => screen.getByPlaceholderText(/Type your name/),
+}
+
+const defaultProps = {
+  placeholder: 'Type your name',
+}
+
+const renderComponent = (props: InputProps = defaultProps) => (
+  render(<Input {...props} />)
+)
+
+test('should render the component without icon', async () => {
+  renderComponent()
+
+  expect(selectors.placeholder()).toBeInTheDocument()
+})
+
+test('should render the component with icon', async () => {
+  const { debug } = renderComponent({
+    icon: <SearchIcon />,
+    placeholder: 'Type your name',
+  })
+
+  debug()
+})
